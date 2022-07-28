0b6dd394598740a35a4370944955e5aeeaf3e044
刘一凡 <1729372667@qq.com>
2022-07-28 21:59:39 +0800

    村庄改与查

--- src/api/index.js
+++ src/api/index.js
@@ -1,9 +1,20 @@
 import request from '../utils/request';
+import post from '../utils/request';
 
 export const fetchData = query => {
     return request({
-        url: './table.json',
+        url: 'http://localhost:8999/create/country/all',
         method: 'get',
         params: query
+    });
+};
+
+export const updateCountry = config => {
+	// CORS problem unsolved. Use Get temporarily (very unsafe)
+    // return post('http://localhost:8999/create/test', config);
+	return request({
+        url: 'http://localhost:8999/create/modify/test',
+        method: 'get',
+        params: config
     });
 };
--- src/views/BaseTable.vue
+++ src/views/BaseTable.vue
@@ -17,33 +17,23 @@
                 <el-button type="primary" icon="el-icon-search" @click="handleSearch">搜索</el-button>
             </div>
             <el-table :data="tableData" border class="table" ref="multipleTable" header-cell-class-name="table-header">
-                <el-table-column prop="id" label="ID" width="55" align="center"></el-table-column>
-                <el-table-column prop="name" label="用户名"></el-table-column>
+                <el-table-column prop="uid" label="用户ID" width="70" align="center"></el-table-column>
+                <el-table-column prop="name" label="申请人"></el-table-column>
                 <el-table-column label="申请创建乡村信息">
-                    <template #default="scope">{{ scope.row.country }}</template>
+                    <template #default="scope">{{ scope.row.cdesc }}</template>
                 </el-table-column>
-                <el-table-column label="头像(查看大图)" align="center">
-                    <template #default="scope">
-                        <el-image class="table-td-thumb" :src="scope.row.thumb" :preview-src-list="[scope.row.thumb]">
-                        </el-image>
-                    </template>
-                </el-table-column>
-                <el-table-column prop="address" label="地址"></el-table-column>
+                <el-table-column prop="location" label="地址"></el-table-column>
                 <el-table-column label="当前审核状态" align="center">
                     <template #default="scope">
                         <el-tag :type="
-                                scope.row.state === '待审核'
+                                scope.row.passed === 0
                                     ? 'warning'
-                                    : scope.row.state === '未通过审核'
-                                    ? 'danger'
-									: scope.row.state === '审核通过'
+									: scope.row.passed === 1
                                     ? 'success'
-									: ''
-                            ">{{ scope.row.state }}</el-tag>
+									: 'danger'
+                            ">{{ scope.row.passed === 0 ? "待审核" : scope.row.passed === 1 ? "审核通过" : "复议中"}}</el-tag>
                     </template>
                 </el-table-column>
-
-                <el-table-column prop="date" label="注册时间"></el-table-column>
                 <el-table-column label="操作" width="180" align="center">
                     <template #default="scope">
                         <el-button type="text" icon="el-icon-circle-plus" @click="handleEdit(scope.$index, scope.row)">查看材料
@@ -66,7 +56,7 @@
                     <el-input v-model="form.name" :disabled="true"></el-input>
                 </el-form-item>
                 <el-form-item label="地址">
-                    <el-input v-model="form.address" :disabled="true"></el-input>
+                    <el-input v-model="form.location"></el-input>
                 </el-form-item>
 				<el-form-item label="证明文件">
 					<el-upload
@@ -89,16 +79,16 @@
 <script>
 import { ref, reactive } from "vue";
 import { ElMessage, ElMessageBox } from "element-plus";
-import { fetchData } from "../api/index";
+import { fetchData, updateCountry } from "../api/index";
 
 export default {
     name: "basetable",
     setup() {
         const query = reactive({
-            address: "",
-            name: "",
-            pageIndex: 1,
-            pageSize: 10,
+            // location: "",
+            // name: "",
+            // pageIndex: 1,
+            // pageSize: 10,
         });
         const tableData = ref([]);
         const pageTotal = ref(0);
@@ -109,6 +99,14 @@ export default {
                 pageTotal.value = res.pageTotal || 50;
             });
         };
+		const submitUpdate = (row, onSuccess, onError) =>{
+			updateCountry(row).then((res)=>{
+				onSuccess();
+			}).catch((err)=>{
+				onError();
+				standardErrorProcedure(err);
+			});
+		}
         getData();
 
         // 查询操作
@@ -121,26 +119,37 @@ export default {
             query.pageIndex = val;
             getData();
         };
+		const standardErrorProcedure = (err) => {
+			console.log('出错啦！');
+			console.log(err);
+			ElMessage.error("操作失败，请检查网络");
+		}
 
         // 删除操作
         const handleDelete = (index) => {
             // 二次确认删除
+			let lastState = tableData.value[index].passed;
             ElMessageBox.confirm("确定要拒绝该请求吗？", "提示", {
                 type: "warning",
             })
                 .then(() => {
-                    ElMessage.success("已拒绝");
-					tableData.value[index].state = "未通过审核";
-                    // tableData.value.splice(index, 1);
+					tableData.value[index].passed = null;
+					submitUpdate(tableData.value[index], 
+					()=>{
+						ElMessage.success('成功拒绝该请求');
+					},
+					()=>{
+						tableData.value[index].passed = lastState;
+					}
+					);
                 })
-                .catch(() => {});
         };
 
         // 表格编辑时弹窗和保存
         const editVisible = ref(false);
         let form = reactive({
             name: "",
-            address: "",
+            location: "",
         });
         let idx = -1;
         const handleEdit = (index, row) => {
@@ -151,12 +160,19 @@ export default {
             editVisible.value = true;
         };
         const saveEdit = () => {
-			tableData.value[idx].state = "审核通过";
-            editVisible.value = false;
-            ElMessage.success(`审核通过`);
-            Object.keys(form).forEach((item) => {
-                tableData.value[idx][item] = form[item];
-            });
+			let lastState = tableData.value[idx].passed; 
+			tableData.value[idx].passed = 1;
+			submitUpdate(tableData.value[idx],
+			()=>{
+				ElMessage.success('审核通过');
+				Object.keys(form).forEach((item) => {
+				    tableData.value[idx][item] = form[item];
+				});
+				editVisible.value = false;
+			},
+			()=>{
+				tableData.value[idx].passed = lastState;
+			});
         };
 
         return {
